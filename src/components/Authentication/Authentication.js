6382ccc1c8d023109ad66ae2179880b5c3445d7f
LahiaOmar <omar.lahia@gmail.com>
2020-05-29 17:25:24 +0000

    first commit : Authentication component files

---
+++ src/components/Authentication/Authentication.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LogIn from '../LogIn'
+import SignIn from '../SignIn'
+
+function Authentication() {
+    return (
+        <div>
+            <LogIn />
+            <SignIn />
+        </div>
+    )
+}
+
+export default Authentication
