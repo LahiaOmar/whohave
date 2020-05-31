00737fc523fed15e8489ed407ec94c6008adcc69
LahiaOmar <omar.lahia@gmail.com>
2020-05-31 12:36:46 +0100

    adding MyModal component

---
+++ src/components/Mymodal/MyModal.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import {Button, Modal, Backdrop} from '@material-ui/core'
+
+function MyModal(props){
+
+    const [open, setOpen] = React.useState(false)
+
+    const handleOpen = ()=>{
+        setOpen(true)
+    }
+
+    const handleClose = ()=>{
+        setOpen(false)
+    }
+
+    return (
+        <React.Fragment>
+            <Button variant="contained" color="secondary" onClick={handleOpen}>
+                {props.btnTitle}
+            </Button>
+            <Modal
+            className="container-modal"
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+            >
+                {props.children}
+            </Modal>
+        </React.Fragment>
+    )
+}
+
+export default MyModal
---
+++ src/components/Mymodal/index.js
@@ -0,0 +1,3 @@
+import MyModal from './MyModal'
+
+export default MyModal
