1c56d1c90b4543313846f0af4db5de0d431e44ef
LahiaOmar <omar.lahia@gmail.com>
2020-06-11 19:49:46 +0100

    add loginContext to consume user login state

--- src/components/Authentication/Authentication.js
+++ src/components/Authentication/Authentication.js
@@ -2,32 +2,39 @@ import React from 'react'
 import LogIn from '../LogIn'
 import SignUp from '../SignUp'
 import MyModal from '../Mymodal'
+import LoginContext from '../ContextAuth'
 
 function Authentication() {
-    
-    const submitSingUp = (data)=>{
-        console.log("submit sign up data : ", data)
-    }
+	
+	const {isLoged, setIsLoged } = React.useContext(LoginContext)
 
-    const submitLogIn = (data)=>{
-        console.log("submition data from login component, data : ", data)
-    }
+	const submitSingUp = (data)=>{
+		console.log("submit sign up data : ", data)
+	}
 
-    return (
-        <div>
-            {true
-            ?<React.Fragment>
-                <MyModal btnTitle="Sign UP">
-                    <SignUp submitSingUp={submitSingUp} />
-                </MyModal>
-                <MyModal btnTitle="Log IN">
-                    <LogIn submitSingUp={submitLogIn}/>
-                </MyModal>
-            </ React.Fragment>
-            :<div>user information</div>
-        }
-        </div>
-    )
+	const submitLogIn = (data)=>{
+		console.log("submition data from login component, data : ", data)
+	}
+
+	return (
+		<div>
+				{isLoged
+					?(
+						<div>user information</div>
+					)
+					:(
+						<React.Fragment>
+							<MyModal btnTitle="Sign Up">
+									<SignUp submitSingUp={submitSingUp} />
+							</MyModal>
+							<MyModal btnTitle="Log In">
+									<LogIn submitSingUp={submitLogIn}/>
+							</MyModal>
+						</React.Fragment>
+					)
+				}
+		</div>
+	)
 }
 
 export default Authentication
