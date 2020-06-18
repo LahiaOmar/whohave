7198be7ebcb38fe520717c862ab0adf9c638deef
LahiaOmar <omar.lahia@gmail.com>
2020-06-18 17:23:38 +0100

    add backend part pf the app, index is the server adn app is the express app

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const users = require('./routes/users')
+
+const app = express()
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('api/auth', users)
+
+module.exports = app
