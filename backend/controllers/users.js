b30b9b6044154e3b7507d7c08ce8ad159a1352e6
LahiaOmar <omar.lahia@gmail.com>
2020-06-19 15:57:34 +0100

    implementation of signup of store and user methods

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,13 +1,33 @@
 const bcrypt = require('bcrypt')
+const userStore = require('../models/userStore')
+const UserWho = require("../models/user")
 
 exports.storeSignUp = (req, res, next)=>{
-  bcrypt.hash(req.body.password, 10)
+  const {password, ...restOfFiled} = req.body
+  bcrypt.hash(password, 10)
     .then(hash=>{
-      console.log("password hash", hash)
+      const currentUserStore = new userStore({
+        ...restOfFiled,
+        password : hash
+      })
+      currentUserStore.save()
+        .then(()=> res.status(201).json({message : "object Created"}))
+        .catch((err)=> res.status(400).json({err}))
     })
     .catch(error => res.status(500).json({error}))
 }
 
 exports.userSignUp = (req, res, next) =>{
-  
+  const {password, ...restOfFiled} = req.body
+  bcrypt.hash(password, 10)
+    .then(hash=>{
+      const currentUser = new UserWho({
+        ...restOfFiled,
+        password : hash
+      })
+      currentUser.save()
+        .then(()=>res.status(201).json({message : "user created !"}))
+        .catch(err=> res.status(400).json({err}))
+    })
+    .catch(err=>res.status(500).json({err}))
 }
