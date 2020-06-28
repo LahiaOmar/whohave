aa887285fde87c67a5312708e884e5a3bb962e0e
LahiaOmar <omar.lahia@gmail.com>
2020-06-28 17:25:22 +0100

    we are no longer using a session

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const users = require('./routes/users')
-
+const mongoos = require("mongoose")
 const app = express()
 
-app.use(express.json())
+mongoos.connect('mongodb://localhost:27017/whohave', { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true })
+  .then(()=> console.log("connection to mongoDB"))
+  .catch((e)=>console.log("error connection to mongoDB", e))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -12,6 +16,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('api/auth', users)
+app.use(express.json())
+
+app.use('/api/auth', users)
 
 module.exports = app
