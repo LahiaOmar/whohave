4d6e1fc7929982d03de9845afebd0fefdb21142f
LahiaOmar <omar.lahia@gmail.com>
2020-07-12 10:48:26 +0100

    refactor this component

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,72 +1,24 @@
 import React from 'react'
 import ReactMapGL,{Marker, GeolocateControl} from 'react-map-gl'
-import {Fab } from '@material-ui/core'
-import RoomIcon from '@material-ui/icons/Room';
 
-function Map(){
 
-	const [viewport, setViewport] = React.useState({
-    width : 'calc(100%)',
-		height : 'calc(100vh - 50px)',
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
-	})
-	const [allMarker, setAllMarker] = React.useState([])
-	const [myPosition, setMyPosition] = React.useState(null)
-	const btnPosition = React.createRef()
-	const key = "pk.eyJ1IjoibGFoaWFvbWFyIiwiYSI6ImNrYjM3a3hqMDA2ZDUycm85NjIxeGZiMTEifQ.cW7NWVSIF_5kJG7_Phfg9A"
-	
-	const addMarker = ({lngLat})=>{
-		const prev = [...allMarker]
-		prev.push(lngLat)
-		setAllMarker(prev)
-	}
-	
-	const userPosition = ({longitude, latitude}, options = {})=>{
-		console.log("in", typeof longitude, latitude)
-		setMyPosition({longitude, latitude})
-		setViewport(prev => ({...prev, longitude : longitude, latitude : latitude, ...options}))
-	}
+function Map(props){
 
-	const handleMyPosition = (event)=>{
-		if(navigator.geolocation){
-			navigator.geolocation.getCurrentPosition((position)=>{
-				 userPosition(position.coords, {zome : 10})
-			})
-		}
-	}
-	const handleDraggableUSerEvent = (coords)=>{
-		console.log(coords)
-		const {lngLat} = coords
-		const longitude = lngLat[0]
-		const latitude = lngLat[1]
-		userPosition({longitude, latitude})		
-	}
+	const [viewport, setViewport] = React.useState(props.viewPort)
+	const key = process.env.REACT_APP_MAP_KEY
+
 	return (
 		<div id="map">
 			<div className="btnPosition">
-				<Fab color="primary" onClick={handleMyPosition}>
-					<RoomIcon />
-				</Fab>
+				
 			</div>
 			<ReactMapGL 
 				{...viewport}
 				mapboxApiAccessToken={key}
 				onViewportChange={nextViewport => {setViewport(nextViewport)}}
-				onClick={(e)=>{
-					addMarker(e)
-					// set position of the store
-				}}>
+			>
 				{
-					myPosition &&
-					<Marker
-						draggable={true}
-						onDragEnd={handleDraggableUSerEvent} 
-						longitude={myPosition.longitude}
-						latitude={myPosition.latitude}>
-						<RoomIcon fontSize="large" />
-				</Marker>
+					props.children
 				}
 			</ReactMapGL>
 		</div >
