07fa3426a08bf15f6b871b7836d54798da81cf3a
LahiaOmar <omar.lahia@gmail.com>
2020-07-19 18:42:04 +0100

    shiftup all state of the map to dashboard and change state handler to reducer pattern

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,31 +1,83 @@
 import React from 'react'
 import {Grid} from '@material-ui/core'
-import UserNavBar from '../UserNavBar'
-import StoresInfo from '../StoresInfo'
 import Map from '../Map'
 import LoginContext from '../ContextAuth'
-import { Redirect } from 'react-router-dom'
+import {UserStore, UserConsumer} from '../UsersMarkers'
+import MyMarker from '../MyMarker'
+import {StoreOpen} from '../ButtonActions'
+import Message from '../Message'
+import { StoreAction, ConsumerAction } from '../UsersActions'
+
+function reducer(state, action){
+	console.log("actual state : ", state)
+	const actionsTypes = action.type.split("&")
+	let newState = {...state}
+	for(let act of actionsTypes){	
+		switch(act){
+			case 'add_marker' : 
+				newState = { ...newState, markers: state.markers.concat(action.marker)}
+				break
+			case 'change_focus_map' :
+				newState = { ...newState,viewPort : {
+					...newState.viewPort,
+					longitude : action.longitude,
+					latitude : action.latitude
+				}}
+				break
+		}
+	}
+	console.log("new State", newState)
+	if(Object.keys(newState).length === 0) throw new Error("dashboard reducer error")
+	return newState
+}
 
 function Dashboard(){
-	const {isLoged} = React.useContext(LoginContext)
-	return (isLoged
-				?(
-					<div id="dashboard">
-						<Grid container spacing={0}>
-							<Grid item xs={2} >
-								<div>Store notificaions</div>
-							</Grid>
-							<Grid item xs={10}> 
-								<Map/>
-							</Grid>
-						</Grid>            
+	const context = React.useContext(LoginContext)
+	const start = {latitude : 31.669746 , longitude :-7.973328}
+	const isExistCoord = context.user.coords.length !== 0
+	const startCoord =  isExistCoord ? context.user.coords : start
+	const [state, dispatch] = React.useReducer(reducer, {
+		viewPort : {
+			width : 'calc(100%)',
+			height : 'calc(100vh - 50px)',
+			...startCoord,
+			zoom: 8
+		},
+		markers : [],
+		userCoords : {
+			...startCoord
+		},
+	})
+
+	return (	
+		<div id="dashboard">
+			<Grid container spacing={0}>
+				<Grid item xs={2} >
+					<div>Store notificaions </div>
+				</Grid>
+				<Grid item xs={10}> 
+					<div className="btnPosition">
+						{
+							context.user.type
+							?<StoreAction
+									dispatch={dispatch}
+									coords={state.userCoords}
+									isExist={isExistCoord}
+								/>
+							:<ConsumerAction 
+									dispatch={dispatch}/>
+						}
 					</div>
-				)
-				:
-				( 
-					<Redirect to="/"/>
-				)
-			)
+					<Map
+						state={state}>
+							{
+								state.markers.map((marker)=>(marker))
+							}
+					</Map>
+				</Grid>
+			</Grid>            
+		</div>
+	)
 }
 
 export default Dashboard
