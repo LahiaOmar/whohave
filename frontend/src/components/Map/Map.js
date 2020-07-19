9856556039a100340fc92cdeaeec953d0faf2421
LahiaOmar <omar.lahia@gmail.com>
2020-07-19 18:46:21 +0100

    shif state to dashborad

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import ReactMapGL,{Marker, GeolocateControl} from 'react-map-gl'
-
+import ReactMapGL from 'react-map-gl'
 
 function Map(props){
-
-	const [viewport, setViewport] = React.useState(props.viewPort)
+	console.log("map ", props)
+	const [viewport, setViewport] = React.useState(props.state.viewPort)
 	const key = process.env.REACT_APP_MAP_KEY
+
+	const handleNextViewPort = (next)=>{
+		console.log("next", next)
+		setViewport(next)
+	}
+	
+	React.useEffect(()=>{
+		if(props.state.viewPort.longitude !== viewport.longitude && 
+			props.state.viewPort.latitude !== viewport.latitude){
+			setViewport(props.state.viewPort)
+		}
+	},[props.state.viewPort])
 
 	return (
 		<div id="map">
-			<div className="btnPosition">
-				
-			</div>
 			<ReactMapGL 
 				{...viewport}
 				mapboxApiAccessToken={key}
-				onViewportChange={nextViewport => {setViewport(nextViewport)}}
+				onViewportChange={nextViewport => {handleNextViewPort(nextViewport)}}
 			>
 				{
 					props.children
