8025555bbbd685bbf00ebd65301959ca3205b63b
LahiaOmar <omar.lahia@gmail.com>
2020-07-19 18:40:08 +0100

    set the initail object of the context

--- frontend/src/components/ContextAuth/LoginContext.js
+++ frontend/src/components/ContextAuth/LoginContext.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
-const LoginContext = React.createContext(null)
+
+const LoginContext = React.createContext({
+  user : {
+    isLoged : false,
+    isStore : false,
+  },
+  setUser : ()=>{}
+})
 
 export default LoginContext
