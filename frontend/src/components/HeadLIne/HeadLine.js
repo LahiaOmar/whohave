b33a440518a7329610aac0c88260fe9a589257fd
LahiaOmar <omar.lahia@gmail.com>
2020-07-27 18:03:12 +0100

    some tests

--- frontend/src/components/HeadLIne/HeadLine.js
+++ frontend/src/components/HeadLIne/HeadLine.js
@@ -2,18 +2,29 @@ import React from 'react'
 import {Grid} from '@material-ui/core'
 
 function HeadLine(){
-    return(
-        <div>
-            <Grid container justify="center" spacing={1} id="head-line">
-                <Grid item id="head-line-info">
-                    some information about the app
-                </Grid>
-                <Grid item id="head-line-start">
-                    logo and btn for start
-                </Grid>
-            </Grid>
-        </div>
-    )
+	return(
+		<Grid container xs={12}>
+			<Grid item xs={6}>
+				<p>whohave</p>
+				<p>Store Owner : </p>
+				<ul>
+					<li>Locate You Store</li>
+					<li>Wait For Client Request</li>
+					<li>dump-up OR dump-down</li>
+					<li>More client ;)</li>
+				</ul>
+				<p>Search a product : </p>
+				<ul>
+					<li>Select type of your product</li>
+					<li>add some others information : (description, image ... )</li>
+					<li>bessa7a bro ;)</li>
+				</ul>
+			</Grid>
+			<Grid item xs={6}>
+				<p>biglo </p>
+			</Grid>
+		</Grid>
+	)
 }
 
 export default HeadLine
