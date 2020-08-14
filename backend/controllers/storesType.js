c55c1f9e17459e3bd52da349c8b9dfd71e33a1ab
LahiaOmar <omar.lahia@gmail.com>
2020-08-14 17:51:28 +0100

    add cnotoller for handling add new store type req

---
+++ backend/controllers/storesType.js
@@ -0,0 +1,20 @@
+const storesType = require('../models/storesType')
+
+exports.get = function(req, res){
+  storesType.find()
+    .then((data)=>{res.status(200).json(data)})
+    .catch((err)=>{res.sendStatus(400)})
+}
+
+exports.add = function(req, res){
+  console.log("req.body", req.body)
+  let {specialty} = req.body
+  specialty = specialty.toLowerCase().trim()
+  const newType = new storesType({specialty})
+  newType.save()
+    .then(()=>{
+      storesType.find()
+        .then((data)=>res.status(200).json(data))
+    })
+    .catch((err)=> res.status(500).json({msg : "server error"}))
+}
