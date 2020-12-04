ecd20a4be40243aca7ea7eab5ab803e62592bdbb
LahiaOmar <omar.lahia@gmail.com>
2020-12-04 15:31:51 +0100

    remove useNotification hook from this file.

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -7,6 +7,7 @@ import {
 	Grid,
 	Toolbar,
 	Link as LinkUi,
+	Badge,
 } from '@material-ui/core'
 import LoginContext from '../ContextAuth'
 import ListOfProduct from '../ListOfProduct'
@@ -25,86 +26,7 @@ import UserMenuList from '../UserMenuList'
 import StoreMenuList from '../StoreMenuList'
 import Message from '../Message'
 
-function useNotifications() {
-	const { userData, type } = React.useContext(LoginContext)
-	const [loading, setLoading] = React.useState(true)
-	const [state, dispatch] = React.useReducer((state, action) => {
-		let newState = [...state]
-		switch (action.type) {
-			case 'CHECK_BYID':
-				newState = newState.map(element => {
-					if (element._id === action.id) {
-						element.isSelected = !element.isSelected
-					}
-					return element
-				})
-				break;
-			case 'CHECK_ALL':
-				newState = newState.map(element => {
-					element.isSelected = action.isSelected
-					return element
-				})
-				break;
-			case 'DELETE':
-				newState = newState.filter((element => !action.idsArr.includes(element._id)))
-				break;
-			case 'ADD':
-				newState.push(action.newNotification)
-				break;
-			case 'UPDATE':
-				newState = action.newNotifications
-				break;
-			default:
-				console.log("error action type")
-		}
-		return newState
-	}, [])
-
-
-	const isConnectedHandler = async () => {
-		const config = {
-			method: 'POST',
-			url: 'http://localhost:4000/api/notifications/get',
-			data: {
-				type: type,
-				userId: userData._id
-			}
-		}
-		const { data } = await Axios(config)
-		setLoading(false);
-		const newNotifications = data.notifications.map(e => {
-			e.isSelected = false
-			return e
-		})
-		dispatch({ type: 'UPDATE', newNotifications })
-	}
-
-	const addNotification = (data) => {
-		console.log("new notification ", data)
-		const newNotification = JSON.parse(data)
-		newNotification.isSelected = false
-		dispatch({ type: 'ADD', newNotification })
-	}
-
-	React.useEffect(() => {
-		const options = {
-			query: {
-				userId: userData._id,
-				type: type
-			}
-		}
-		const pathSocket = process.env.REACT_APP_PATH_SOCKET
-		const socket = io(pathSocket, options)
-		socket.on('connect', () => isConnectedHandler())
-		socket.on('newNotification', addNotification)
-
-		return () => {
-			socket.close()
-		}
-	}, [])
-
-	return [state, dispatch, loading]
-}
+import useNotifications from '../useHooks/UseNotifications'
 
 const Dashboard = () => {
 	const context = React.useContext(LoginContext)
@@ -124,13 +46,11 @@ const Dashboard = () => {
 						<Button color="inherit">
 							<DehazeIcon />
 						</Button>
-						<Button color="inherit">
+						<Badge badgeContent={10} color="secondary">
 							<NotificationsIcon />
-
-						</Button>
+						</Badge>
 						<Button color="inherit">
 							<ExitToAppIcon />
-
 						</Button>
 					</Toolbar>
 				</AppBar>
