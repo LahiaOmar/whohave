98e5bb5b569ee1f36499874198966aeec114dc39
LahiaOmar <omar.lahia@gmail.com>
2021-01-03 16:57:58 +0100

    add cors and cookieparser middleware and some syntaxe correction

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoos = require("mongoose")
 const socket = require("socket.io")
 const http = require("http")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 const users = require('./routes/users')
 const products = require('./routes/products')
@@ -22,9 +24,10 @@ const normalizePort = val => {
   }
   return false;
 };
-
 const PORT = normalizePort(process.env.PORT || '4000');
 const app = express()
+const httpServer = http.Server(app)
+const io = socket(httpServer, { origins: '*:*' })
 
 mongoos.connect('mongodb://localhost:27017/whohave', {
   useNewUrlParser: true,
@@ -33,15 +36,10 @@ mongoos.connect('mongodb://localhost:27017/whohave', {
   .then(() => console.log("connection to mongoDB"))
   .catch((e) => console.log("error connection to mongoDB", e))
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use(cors())
 
-const http_server = http.Server(app)
-const io = socket(http_server, { origins: '*:*' })
+
+app.use(cookieParser())
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -50,11 +48,11 @@ app.use((req, res, next) => {
 })
 
 io.on('connection', connections.socketConnected)
-app.use('/api/', users)
+
 app.use('/api/user', users)
 app.use('/api/products', products)
 app.use('/api/positions', positions)
 app.use('/api/storesType', storesType)
 app.use('/api/notifications', notifications)
 
-http_server.listen(PORT, () => console.log(`app listen on port ${PORT} ...`)) 
+httpServer.listen(PORT, () => console.log(`app listen on port ${PORT} ...`)) 
