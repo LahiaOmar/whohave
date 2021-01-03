869ef94313d609121235a19339f530f1df2ee972
LahiaOmar <omar.lahia@gmail.com>
2021-01-03 16:59:07 +0100

    fix bug : name of model

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,19 +2,15 @@ const express = require("express")
 const router = express.Router()
 const jwt = require("jsonwebtoken")
 const userStore = require("../models/userStore")
-const user = require("../models/user")
+const userConsumer = require("../models/user")
+
 module.exports = async (req, res, next) => {
   const { cookies } = req
   const { userType } = req.body
-  console.log("auth.js : usertype ", userType, cookies)
   try {
-    console.log("decodeding token ...")
     const decodeToken = await jwt.verify(cookies.token, "RANDOM_SECRECT_KEY")
-    console.log("decoded token ", decodeToken)
-    const model = userType ? userStore : user
-    console.log("model ", model)
+    const model = userType ? userStore : userConsumer
     const user = await model.findById({ _id: decodeToken.userId })
-    console.log("user next", user)
     if (user) {
       res.locals.userId = decodeToken.userId
       res.locals.userType = userType
@@ -25,6 +21,7 @@ module.exports = async (req, res, next) => {
     }
   }
   catch (e) {
+    console.log("e : ", e)
     res.status(401).json({ valideToken: false })
   }
 }
