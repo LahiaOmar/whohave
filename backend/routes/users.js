5061f1926368ce581076d8c2104dd6b1b6b829cd
LahiaOmar <omar.lahia@gmail.com>
2021-01-03 17:07:12 +0100

    add route for set password users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const users = require('../controllers/users')
 const router = express.Router()
+const authentications = require("../middleware/auth")
 
 router.post('/auth/storeSignup', users.storeSignUp)
 router.post('/auth/userSignup', users.userSignUp)
 router.post('/auth/login', users.userLogin)
+router.post('/auth/setPassword', users.setPassword)
 router.post('/updateUser', users.userSetInformation)
+router.post('/verify', authentications, users.verify)
 module.exports = router
