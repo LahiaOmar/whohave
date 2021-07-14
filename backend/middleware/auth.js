fbb57ba5b73f41199a97b8ac02ccad35b67bbc6e
LahiaOmar <omar.lahia@gmail.com>
2021-07-14 10:23:49 +0100

    refactoring, and using gttp-status codes lib

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,21 +1,16 @@
-const express = require("express")
-const router = express.Router()
 const jwt = require("jsonwebtoken")
-const userStore = require("../models/userStore")
-const userConsumer = require("../models/user")
+const { UNAUTHORIZED } = require('http-status')
 
 module.exports = async (req, res, next) => {
-  const { cookies } = req
   try {
+    const { cookies } = req
     const decodeToken = await jwt.verify(cookies.token, "RANDOM_SECRECT_KEY")
     const { userId, userType } = decodeToken
-    console.log("decoded token", decodeToken)
     res.locals.userId = userId
     res.locals.userType = userType
     next()
   }
-  catch (e) {
-    console.log("user token not valide #############################################################")
-    res.status(401).json({ valideToken: false })
+  catch (ex) {
+    res.status(UNAUTHORIZED).json({ error: ex.message, valideToken: false })
   }
 }
