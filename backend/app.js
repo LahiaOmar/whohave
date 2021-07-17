307f8d18713750e9ddaa090f41c8d93fd40f0090
LahiaOmar <omar.lahia@gmail.com>
2021-07-17 11:02:16 +0100

    add env file for the backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require("http")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+require('dotenv').config()
 
 const users = require('./routes/users')
 const products = require('./routes/products')
